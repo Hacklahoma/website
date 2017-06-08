ebb56b76e3b2c15b58bccb954044a947a0dbaa60
twalker1998 <twalker1998@ou.edu>
2017-06-07 21:57:27 -0500

    optimized highlighting of nav links
    not sure why but the link underline lags when on mac safari

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,20 +3,18 @@
 //
 //  scripts.js
 //  HacklahomaWebsite
-//  
-//  Created by Tyler Walker on 2017-06-01.
-//  Copyright 2017 Tyler Walker and Hacklahoma. All rights reserved.
 // 
 
-// Execute fixNavBar() when the window is scrolled. 
-document.body.onscroll = function() {highlightCurrentSection();};
+// Global variable for the top of the nav bar.
+// (Needs to be global to enable changes when the window resizes).
+var stickyNavTop = $('#navBar').offset().top;
 
-/*
+/**
 	Fixes the navBar at the top of the screen when the user's screen is 
 	in the content section, and at the bottom of the header when it isn't.
 */
 $(document).ready(function() {
-	var stickyNavTop = $('#navBar').offset().top;
+	stickyNavTop = $('#navBar').offset().top;
 	
 	var stickyNav = function() {
 		var scrollTop = $(window).scrollTop();
@@ -30,66 +28,66 @@ $(document).ready(function() {
 	
 	stickyNav();
 	
-	$(window).scroll(function() {
-		stickyNav();
+	$(window).on({
+		scroll: function() {
+			stickyNav();
+		}, resize: function() {
+			if( $('#navBar').attr('class') == "absolute fixed") {
+				// do nothing
+			} else {
+				stickyNavTop = $('#navBar').offset().top;
+			}
+		}
 	});
 });
 
-/*
+/**
 	Highlights the link in the navbar of the section currently displayed on the
 	user's screen.
+	
+	Code from Melvin de Jong.
+	https://theme.co/apex/forums/topic/one-page-navigation-navbar-to-highlight-section/#post-718740
 */
-function highlightCurrentSection() {
-	// The position of the page where the user has scrolled.
-	var scrollTop = document.body.scrollTop;
-	
-	// The top of the About section.
-	var aboutTop = document.getElementById("about").getBoundingClientRect().top + scrollTop;
-	
-	// The top of the FAQ section.
-	var faqTop = document.getElementById("faq").getBoundingClientRect().top + scrollTop;
-	
-	// The top of the Agenda section.
-	var agendaTop = document.getElementById("agenda").getBoundingClientRect().top + scrollTop;
-	
-	// The top of the Sponsors section.
-	var sponsorsTop = document.getElementById("sponsors").getBoundingClientRect().top + scrollTop;
-	
-	// The top of the Judges section.
-	var judgesTop = document.getElementById("judges").getBoundingClientRect().top + scrollTop;
-	
-	// Make the link active based on the section the user is looking at.
-	if(scrollTop < aboutTop) {
-		$("a[id='aboutLink']").removeClass('active');
-	}
-	if(scrollTop >= aboutTop && scrollTop < faqTop) {
-		$("a[id$='Link']").removeClass('active');
-		document.getElementById("aboutLink").className = "active";
-	} else if(scrollTop >= faqTop && scrollTop < agendaTop) {
-		$("a[id$='Link']").removeClass('active');
-		document.getElementById("faqLink").className = "active";
-	} else if(scrollTop >= agendaTop && scrollTop < sponsorsTop) {
-		$("a[id$='Link']").removeClass('active');
-		document.getElementById("agendaLink").className = "active";
-	} else if(scrollTop >= sponsorsTop && scrollTop < judgesTop) {
-		$("a[id$='Link']").removeClass('active');
-		document.getElementById("sponsorsLink").className = "active";
-	} else if(scrollTop >= judgesTop) {
-		$("a[id$='Link']").removeClass('active');
-		document.getElementById("judgesLink").className = "active";
-	}
-}
+$(document).ready(function($) {
+	/*
+     * This part handles the highlighting functionality.
+     * We use the scroll functionality again, some array creation and 
+     * manipulation, class adding and class removing, and conditional testing
+     */
+    var aChildren = $("a[id$='Link']").toArray();
+    
+    var aArray = []; // create the empty aArray
+    for(var i = 0; i < aChildren.length; i++) {
+    	var aChild = aChildren[i];
+    	var ahref = $(aChild).attr('href');
+    	aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+    
+    $(window).scroll(function() {
+    	var windowPos = $(window).scrollTop(); // get the offset of the window from the top of the page
+    	var windowHeight = $(window).height(); // get the height of the window
+    	var docHeight = $(document).height();
+    	
+    	for(var i = 0; i < aArray.length; i++) {
+    		var theID = aArray[i];
+    		var divPos = $(theID).offset().top; // get the offset of the div from the top of the page
+    		divPos = divPos - 50; // fix for navBar height
+    		var divHeight = $(theID).height(); // get the height of the div in question
+    		divHeight = divHeight + 50; // correction for our previous adjustment so that the end of the div is calculated properly
+    		
+    		if(windowPos >= divPos && windowPos < (divPos + divHeight)) {
+    			$("a[href=" + theID + "]").addClass("active");
+    			$("a[href=" + theID + "]").addClass("underline");
+    		} else {
+    			$("a[href=" + theID + "]").removeClass("active");
+    			$("a[href=" + theID + "]").removeClass("underline");
+    		}
+    	}
+    });
+});
 
-function resizeHeader() {
-	var windowWidth = $(window).width();
-	var windowHeight = $(window).height();
-	
-	$('#header').css(width, windowWidth);
-	$('#header').css(height, windowHeight);
-}
-
-/*
-	Animate scrolling to each section.
+/**
+	Animates scrolling to each section.
 	
 	Code from Valentin Sarychev.
 	http://jsfiddle.net/dizel3d/1eamwt4e/
