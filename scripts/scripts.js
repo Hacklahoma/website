badb3e94df500f061f10c39aae4d2fdba3456e0f
jwlarocque <jwlarocque@gmail.com>
2017-06-19 20:05:54 -0500

    Discarded separate mobile menu, integrated mobile version with normal scrolly menu.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,33 +1,37 @@
-// 
+//
 //  This file contains any scripts that will be run on the Hacklahoma website.
 //
 //  scripts.js
 //  HacklahomaWebsite
-// 
+//
 
 // Global variable for the top of the nav bar.
 // (Needs to be global to enable changes when the window resizes).
 var stickyNavTop = $('#navBar').offset().top;
 
 /**
-	Fixes the navBar at the top of the screen when the user's screen is 
+	Fixes the navBar at the top of the screen when the user's screen is
 	in the content section, and at the bottom of the header when it isn't.
 */
 $(document).ready(function() {
 	stickyNavTop = $('#navBar').offset().top;
-	
+
 	var stickyNav = function() {
 		var scrollTop = $(window).scrollTop();
-		
-		if(scrollTop > stickyNavTop) {
+
+        // modified for "mobile" browsers
+        // (width less than 750px)
+        if (window.innerWidth < 750) {
+            // do nothing (shh it's fine)
+        } else if(scrollTop > stickyNavTop) {
 			$('#navBar').addClass('fixed');
 		} else {
 			$('#navBar').removeClass('fixed');
 		}
 	};
-	
+
 	stickyNav();
-	
+
 	$(window).on({
 		scroll: function() {
 			stickyNav();
@@ -44,37 +48,37 @@ $(document).ready(function() {
 /**
 	Highlights the link in the navbar of the section currently displayed on the
 	user's screen.
-	
+
 	Code from Melvin de Jong.
 	https://theme.co/apex/forums/topic/one-page-navigation-navbar-to-highlight-section/#post-718740
 */
 $(document).ready(function($) {
 	/*
      * This part handles the highlighting functionality.
-     * We use the scroll functionality again, some array creation and 
+     * We use the scroll functionality again, some array creation and
      * manipulation, class adding and class removing, and conditional testing
      */
     var aChildren = $("a[id$='Link']").toArray();
-    
+
     var aArray = []; // create the empty aArray
     for(var i = 0; i < aChildren.length; i++) {
     	var aChild = aChildren[i];
     	var ahref = $(aChild).attr('href');
     	aArray.push(ahref);
     } // this for loop fills the aArray with attribute href values
-    
+
     $(window).scroll(function() {
     	var windowPos = $(window).scrollTop(); // get the offset of the window from the top of the page
     	var windowHeight = $(window).height(); // get the height of the window
     	var docHeight = $(document).height();
-    	
+
     	for(var i = 0; i < aArray.length; i++) {
     		var theID = aArray[i];
     		var divPos = $(theID).offset().top; // get the offset of the div from the top of the page
     		divPos = divPos - 50; // fix for navBar height
     		var divHeight = $(theID).height(); // get the height of the div in question
     		divHeight = divHeight + 50; // correction for our previous adjustment so that the end of the div is calculated properly
-    		
+
     		if(windowPos >= divPos && windowPos < (divPos + divHeight)) {
     			$("a[href=" + theID + "]").addClass("active");
     			$("a[href=" + theID + "]").addClass("underline");
@@ -88,7 +92,7 @@ $(document).ready(function($) {
 
 /**
 	Animates scrolling to each section.
-	
+
 	Code from Valentin Sarychev.
 	http://jsfiddle.net/dizel3d/1eamwt4e/
 */
@@ -97,19 +101,19 @@ $(document).ready(function($) {
 $(document).on('click', 'a[href^="#"]', function(e) {
 	// Target element ID
 	var id = $(this).attr('href');
-	
+
 	// Target element
 	var $id = $(id);
 	if($id.length == 0) {
 		return;
 	}
-	
+
 	// Prevent standard hash navigation (avoid blinking in IE)
 	e.preventDefault();
-	
+
 	// Top position relative to the document
 	var pos = $id.offset().top;
-	
+
 	// Animated top scrolling
 	$('body, html').animate({scrollTop: pos}, 500);
 });
